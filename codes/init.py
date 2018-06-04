e1ace5d3c8cd1b51dad145e8c9cb381ebed09007
claudioalvesmonteiro <claudiomonteiro@protonmail.com>
2018-06-04 14:33:43 -0300

    att structure and data

--- codes/init.py
+++ codes/init.py
@@ -11,54 +11,47 @@ Data: 2018-05-21
 Copyright(c) 2018 Claudio Luis Alves Monteiro
 """
 
-#========== importar usuarios e funcoes ==========#
-
 # importar funcionalidades
 import os
 os.chdir("/home/pacha/Documents/git_projects/cade-a-agua/codes")
-from observador_da_agua import *
 from processing_codes import *
 
 # visualizar e mudar diretorio de trabalho
 cwd = os.getcwd()
 os.chdir("/home/pacha/Documents/git_projects/cade-a-agua/")
 
-#=== usuarios ===#
-listaUser = open("data/usuariosChaves.txt", 'r') # abrir arquivo
-listaUser = listaUser.readlines() # ler linhas
-usuarios = removeCaracter(listaUser, "\n") # remover \#
-usuarios = listaDict(usuarios) # transformar em dicionario
+#================== usuarios ================#
+lista_user_data = open("data/usuariosData.txt", 'r') # abrir arquivo
+user_data = lista_user_data.readlines() # ler linhas
+user_data = removeCaracter(user_data, "\n") # remover \n
+user_data = maniUserData(user_data) # lista de infos
 
-#=== reclamacoes ===#
-listaReclama = open("data/reclamacoes.txt", 'r+') # abrir arquivo
-reclamacoes = listaReclama.readlines() # ler linhas
+#================ reclamacoes =================#
+lista_reclama = open("data/reclamacoes.txt", 'r+') # abrir arquivo
+reclamacoes = lista_reclama.readlines() # ler linhas
 reclamacoes = removeCaracter(reclamacoes, "\n") # remover \#
-print(reclamacoes)
 
 #========= INICIO ==========#
 
 # mostraR o que eh
-print("O 'Cadê a Água' é um programa para realizar reclamações sobre o abastecimento de água em sua cidade. Qualquer pessoas pode inserir as informações. Garantimos seu anonimato")
+print("O 'Cadê a Água' é uma plataforma para realizar reclamações sobre o abastecimento de água em sua cidade.\nQualquer pessoa pode inserir as informações. Garantimos seu anonimato.")
 
 # input user info
-print("Precisamos apenas de algumas informações para continuar")
-nomeUser = input("Qual o seu nome? ")
-cpfUser = input("Qual o seu CPF? ")
-emailUser = input("Qual seu email? ")
+print("\nPrecisamos apenas de algumas informações para continuar:")
+user_nome = input("Qual o seu nome? ")
+user_cpf = input("Qual o seu CPF? ")
+user_email = input("Qual seu email? ")
 
 #========= USUARIO SIMPLES =========#
 if cpfUser not in usuarios:
-    reclamacao = []
-    print("Olá ", nomeUser, "Selecione o tipo de reclamação que você deseja fazer:\n1-Observei um vazamento de água na rua!\n2-Observei uma ligação clandestina (jacaré) na rede de abastecimento!\n3-Tá faltando água na minha casa!")
-    reclama = str(int(input("Digite o número da reclamação: ")))
-    localiza = input("Insira o endereço em que você observou o problema (bairro, rua, número da casa à frente): ")
-    codReclama = str(int(reclamacoes[len(reclamacoes)-4])+1) # codigo da ultima reclamacao registrada + 1
-    reclamacao = [codReclama, cpfUser, reclama, localiza]
+    reclamacao = reclamacao(reclamacoes, nomeUser, cpfUser)
+    for info in reclamacao:
+        reclamacoes.append(info)
 
 #========= USUARIO OBSERVADOR =========#
 #if cpfUser in usuarios:
 #    quebra = false
-#    senhaUser = input("Olá ", nomeUser, " vimos que você é um observador da água. Para entrar na sua conta insira sua senha: ")
+#    senhaUser = input("Olá ", user_nome, " vimos que você é um observador da água. Para entrar na sua conta insira sua senha: ")
 #    while quebra == false:
 #        if senhaUser == usuarios[cpfUser]:
 #            menuObservador():
@@ -70,7 +63,14 @@ if cpfUser not in usuarios:
 
 #==== reclamacoes ====#
 reclamacaoStr = plusText(reclamacao) # transformar lista em string com '\n'
-listaReclama.write(reclamacaoStr) # escrever as relcamacoes
-listaReclama.close() # fechar arquivo
+lista_reclama.write(reclamacaoStr) # escrever as relcamacoes
+lista_reclama.close() # fechar arquivo
 
-#==== usuarios ====
+#==== usuarios ====#
+if cpfUser in user_data:
+    user_data_att = atualizarUser(user_data)
+lista_user_data.write(user_data_att) # escrever as relcamacoes
+lista_user_data.close() # fechar arquivo
+
+#==== mensagem final ====#
+print("\nAgradecemos sua colaboração!\nSe você deseja se tornar um(a) observador(a) da água... chega mais")
--- codes/processing_codes.py
+++ codes/processing_codes.py
@@ -10,7 +10,33 @@ Data: 2018-05-21
 
 Copyright(c) 2018 Claudio Luis Alves Monteiro
 """
+#============ Input Reclamacao ===========#
+def reclamacao(reclamacoes, user_nome, cpf_user):
+    reclamacao = []
+    print("Olá ", user_nome, "Selecione o tipo de reclamação que você deseja fazer:\n1-Observei um vazamento de água na rua!\n2-Observei uma ligação clandestina (jacaré) na rede de abastecimento!\n3-Tá faltando água na minha casa!")
+    reclama = str(int(input("Digite o número da reclamação: ")))
+    localiza = input("Insira o endereço em que você observou o problema (bairro, rua, número da casa à frente): ")
+    codReclama = str(int(reclamacoes[len(reclamacoes)-4])+1) # codigo da ultima reclamacao registrada + 1
+    reclamacao = [codReclama, cpf_user, reclama, localiza]
+    return reclamacao
 
+#============ PROCESSANDO AS INFOS =================#
+
+#======== manipular dados usuarios =========#
+def maniUserData(user_data):
+    listaUser = []
+    for usuario in user_data:
+        usuarioInfo = ""
+        for caracter in usuario:
+            if caracter == ";":
+                listaUser.append(usuarioInfo)
+                usuarioInfo = ""
+            else:
+                usuarioInfo += caracter
+    return listaUser
+
+
+#============== Ferramentas Gerais ===============#
 
 # retorna uma lista de objetos, sem um determinado caracter
 def removeCaracter(lista,caracter):
@@ -23,15 +49,6 @@ def removeCaracter(lista,caracter):
         listaFinal.append(objetoMani)
     return listaFinal
 
-# trasnforma lista em dicionario em pares
-def listaDict(lista):
-    cont = 0
-    dicionario = {}
-    while (cont*2+1) < len(lista):
-        dicionario[lista[cont*2]] = lista[cont*2+1]
-        cont += 1
-    return dicionario
-
 # acrescenta texto ao fim de cada objeto numa lista
 def plusText(lista):
     stringLista = ""
@@ -41,4 +58,25 @@ def plusText(lista):
         stringLista += casoNovo
     return stringLista
 
-# automatizar
+#================= FUNCIONALIDADES OBSERVADOR ==============#
+
+#========== Visualizar Hanking =========#
+#
+
+#========== Visualizar Infos Pessoais =========#
+
+#========== Baixar base de dados das Reclamacoes =========#
+
+#========== MENU OBSERVADOR ============#
+#def menuObservador():
+#    senhaUser = input("Olá ", nomeUser, " vimos que você é um observador da água. Para entrar na sua conta insira sua senha: ")
+#    if senhaUser
+
+# trasnforma lista em dicionario em pares
+#def dictUserCount(lista):
+#    cont = 0
+#    dicionario = {}
+#    while (cont*2+1) < len(lista):
+#        dicionario[lista[cont*2]] = lista[cont*2+1]
+#        cont += 1
+#    return dicionario
