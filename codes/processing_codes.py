8c184c58e3f013a0e69a9443255ef99344cf87f6
Cláudio Monteiro <claudiomonteiro@protonmail.com>
2018-06-14 21:15:44 -0300

    Update processing_codes.py

--- codes/processing_codes.py
+++ codes/processing_codes.py
@@ -1,12 +1,10 @@
 """
 UNIVERSIDADE FEDERAL DE PERNAMBUCO (UFPE) (https://www.ufpe.br/)
-CENTRO DE FILOSOFIA E CIENCIAS HUMANAS
-Graduando em Ciencia Politica
 IF968 - Programacao I
 
 Autor(a): Claudio Luis Alves Monteiro
 Email: claudiomonteirol.a@gmail.com
-Data: 2018-05-21
+Data: 2018.1
 
 Copyright(c) 2018 Claudio Luis Alves Monteiro
 """
